4763ecd70bd7bac86b6f78f8415a5b714489cba0
Lorenzo Pisani <zeelot3k@gmail.com>
2012-11-10 18:09:33 -0800

    rename to index.php so it works with the VM (we don't actually have php)

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>JS Mage</title>
+	<script>
+		var require = {
+			deps: ['app'],
+			//By default load any module IDs from js/lib
+			baseUrl: 'js',
+			urlArgs: 'bust=' + (new Date()).getTime(),
+			shim: {
+				'lib/backbone': {
+					deps: ['lib/underscore', 'lib/jquery']
+				}
+			}
+		};
+	</script>
+	<script src="js/lib/require.js"></script>
+	<script type="html/mustache" class="template" data-path="layout/default">
+		<div id="page">
+			<h1>{{pageTitle}}</h1>
+			<div id="mainContent">
+				{{{body}}}
+			<div>
+		</div>
+	</script>
+	<script type="html/mustache" class="template" data-path="page/home">
+		<p>{{message}}</p>
+	</script>
+</head>
+<body>
+	
+</body>
+</html>
