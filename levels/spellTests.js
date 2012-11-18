3593860492df17c15d5799f843211bd738517bf8
Matthew Hokanson <m@h0ke.com>
2012-11-18 01:52:40 -0800

    next three levels

--- levels/spellTests.js
+++ levels/spellTests.js
@@ -14,5 +14,40 @@ var spellTests =
 
 		assertEquals(typeof moniker, 'string')
 		assertEquals('Merlin', moniker);
-	}
+	},
+
+	level03: function(exp)
+	{
+		var res = eval(exp);
+
+		assertEquals(typeof res, 'string')
+		assertEquals('those dates will cost 190 gold!', res);
+	},
+
+	level04: function(exp)
+	{
+		delete window.calmReply; 
+
+		$('head').append('<script>'+exp+'</script>');
+
+		assertEquals(typeof calmReply, 'string')
+		assertEquals('those dates will cost 190 gold.', calmReply);
+	},
+
+	level05: function(exp)
+	{
+		var checkImplemented = exp.indexOf('???');
+
+		if (checkImplemented !== -1)
+		{
+			throw {type:"SpellTest", message:getFailureTerm() + " spell: This spell was not implemented."}
+		}
+
+		$('head').append('<script>'+exp+'</script>');
+
+		assertEquals(typeof haggle, 'function')
+
+		var res = haggle(150);
+		assertEquals(res, 145);
+	},
 };
